26a1a863bea116d3ee85755d8d7c63fa54954bfc
Tyler Cash <12643375+Tyler-Cash@users.noreply.github.com>
2023-09-14 12:38:13 +1000

    Initial commit of files from Spring Initializr

---
+++ src/main/java/dev/tylercash/takehome/interview687e214f/Interview687e214fApplication.java
@@ -0,0 +1,12 @@
+package dev.tylercash.takehome.interview687e214f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Interview687e214fApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Interview687e214fApplication.class, args);
+    }
+}
